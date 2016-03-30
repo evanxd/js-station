ddbadea16bb63b42facf104242564b061f855817
Evan Xd <itoyxd@gmail.com>
2016-03-30 14:38:00 +0800

    Add error handler for post request

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var POST_API = 'http://localhost:3000/sensors/' + config.sensorKey + '/data';
 
 serialDevice.on('data', function(data) {
   request.post(POST_API, {
-    form: { pm25Index: data.pm2dot5 }
+    form: { pm25Index: data.pm25 }
+  }, function(err, response, body) {
+    err && console.error(err);
+    body && console.log(body);
   });
-  console.log('Sent data to SensorWeb.');
   console.log('PM2.5 index data: ' + data.pm25);
   console.log('PM1.0 index data: ' + data.pm10);
   console.log('PM10 index data: ' + data.pm100);
